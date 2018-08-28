fd6bc855b4074894314993c4a048f94ab22bfa1a
AKASH KAMBLE <kambleakashg@gmail.com>
2018-08-28 23:35:00 +0530

    R1D005

---
+++ R1D005/README.md
@@ -0,0 +1,4 @@
+Added react devtools from--
+  https://github.com/jhen0409/react-native-debugger/blob/master/docs/redux-devtools-integration.md
+Addes react native vector icons from --
+  https://github.com/oblador/react-native-vector-icons
---
+++ R1D005/index.js
@@ -0,0 +1,14 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { Provider } from 'react-redux';
+import React from 'react';
+import configureStore from './src/store/configureStore';
+
+const store  = configureStore();
+const RNRedux = () => (
+  <Provider store= {store}>
+    <App/>
+  </Provider>
+);
+
+AppRegistry.registerComponent('rncourse', () => RNRedux );
