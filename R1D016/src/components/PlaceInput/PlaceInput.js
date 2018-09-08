75352c41d367fff06b625c8151ae7eaae82db5bf
AKASH KAMBLE <kambleakashg@gmail.com>
2018-09-08 23:14:05 +0530

    R1D16

---
+++ R1D016/src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
+const PlaceInput = props => (
+  <DefaultInput 
+        placeholder="Place Name" 
+        value={props.placeName} 
+        onChangeText={props.onChangeText}/>
+
+);
+
+export default PlaceInput;
