18a143090579f7d3deeb3b40cf6c128162a97761
AKASH KAMBLE <kambleakashg@gmail.com>
2018-09-09 13:12:48 +0530

    R1D17

---
+++ R1D017/src/screens/MainTabs/startMainTabs.js
@@ -0,0 +1,55 @@
+import { Navigation } from "react-native-navigation";
+import Icon from "react-native-vector-icons/Ionicons";
+import { Platform } from "react-native";
+const startTabs = () => {
+  Promise.all([
+    Icon.getImageSource(Platform.OS === "android" ? "md-map" : "ios-map", 30),
+    Icon.getImageSource(
+      Platform.OS === "android" ? "md-share" : "ios-share",
+      30
+    ),
+    Icon.getImageSource(Platform.OS === "android" ? "md-menu" : "ios-menu", 30)
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: "awesome-places.FindPlaceScreen",
+          label: "Find Place",
+          title: "Find Place",
+          icon: sources[0],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                title: "Menu",
+                icon: sources[2],
+                id: "sideDrawerToggle"
+              }
+            ]
+          }
+        },
+        {
+          screen: "awesome-places.SharePlaceScreen",
+          label: "Share Place",
+          title: "Share Place",
+          icon: sources[1],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                title: "Menu",
+                icon: sources[2],
+                id: "sideDrawerToggle"
+              }
+            ]
+          }
+        }
+      ],
+      drawer: {
+        left: {
+          screen: "awesome-places.SideDrawer"
+        }
+      }
+    });
+  });
+};
+
+export default startTabs;
